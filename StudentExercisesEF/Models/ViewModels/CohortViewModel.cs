6a664fa048615d9dedbd948e444d63612be77660
Meg Cruzen <megcruzen@gmail.com>
2019-04-21 12:00:36 -0500

    Added Cohorts section

---
+++ StudentExercisesEF/Models/ViewModels/CohortViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExercisesEF.Models.ViewModels
+{
+    public class CohortViewModel
+    {
+        public Cohort Cohort { get; set; }
+        public Student Student { get; set; }
+        public Instructor Instructor { get; set; }
+    }
+}
